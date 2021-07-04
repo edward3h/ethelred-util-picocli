34decb1e1adcf79e6cc5f3ed6fae7ca35e754f7d
edward3h <jaq+github@ethelred.org>
2021-07-04 07:56:35 -0400

    initial commit

---
+++ lib/src/main/java/org/ethelred/util/picocli/defaults/EnvironmentDefaultValueProvider.java
@@ -0,0 +1,32 @@
+package org.ethelred.util.picocli.defaults;
+
+import com.google.common.annotations.VisibleForTesting;
+import picocli.CommandLine.IDefaultValueProvider;
+import picocli.CommandLine.Model.ArgSpec;
+import picocli.CommandLine.Model.CommandSpec;
+import picocli.CommandLine.Model.OptionSpec;
+
+import java.util.function.Function;
+
+public class EnvironmentDefaultValueProvider implements IDefaultValueProvider {
+    @VisibleForTesting
+    protected static Function<String, String> envLoader = System::getenv;
+
+    @Override
+    public String defaultValue(ArgSpec argSpec) throws Exception {
+        if (argSpec.isOption()) {
+            OptionSpec option = (OptionSpec) argSpec;
+            CommandSpec command = option.command();
+            String name = _normalize(command.name() + "-" + option.longestName());
+            return envLoader.apply(name);
+        }
+        return null;
+    }
+
+    private String _normalize(String input) {
+        return input.toUpperCase()
+            .replaceAll("^\\W*", "")
+            .replaceAll("\\W+", "_");
+    }
+
+}
